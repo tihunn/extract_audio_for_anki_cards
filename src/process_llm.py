de395308ce1607a6c55af5395ffb2eef946d05b1
tihunn <74502721+tihunn@users.noreply.github.com>
2026-06-11 13:33:50 +0300

    add grammar points

--- src/process_llm.py
+++ src/process_llm.py
@@ -1,10 +1,11 @@
 ﻿from pathlib import Path
 from config_loader import load_config
 import json
-from typing import Dict, Any, Optional
+from typing import Dict, Any
 import os
 from rich.console import Console
-import subprocess
+import re
+import pyperclip
 
 
 def compact_json_content(json_path: Path, remove_empty: bool = True) -> Dict[str, Any]:
@@ -41,6 +42,7 @@ def compact_json_content(json_path: Path, remove_empty: bool = True) -> Dict[str
     compacted_data = compact_dict(data)
     return compacted_data
 
+
 def read_lrc_file(lrc_path: Path) -> str:
     """Читает LRC файл с текстом песни"""
     if not lrc_path.exists():
@@ -50,6 +52,7 @@ def read_lrc_file(lrc_path: Path) -> str:
         content = f.read().strip()
     
     return content
+
 
 def replace_prompt_placeholder(
     search_phrase: str,
@@ -71,6 +74,7 @@ def replace_prompt_placeholder(
         return file_text
 
     return main_prompt.replace(search_phrase, file_text)
+
 
 def create_unified_prompt(
     words_json_path: Path,
@@ -105,7 +109,6 @@ def create_unified_prompt(
     except Exception as e:
         lrc_content = f"Ошибка загрузки LRC: {str(e)}"
 
-
     ready_prompt = replace_prompt_placeholder(
         "{word.json}",
         extract_promt_gpt,
@@ -120,7 +123,6 @@ def create_unified_prompt(
 
     return ready_prompt
 
-import re
 
 def clean_lrc_text(lrc_text: str) -> str:
     """
@@ -149,6 +151,7 @@ def clean_lrc_text(lrc_text: str) -> str:
     ]
 
     return "\n".join(lines)
+
 
 def extract_words_batch(
     json_path: Path,
@@ -186,79 +189,94 @@ def extract_words_batch(
     return text, next_index, has_more
 
 
+def extract_clipboard_content():
+    clipboard_content = pyperclip.paste()
 
-def semi_manual_processing(output_dir: Path, words_json_path: Path, lrc_path: Path):
+    try:
+        data = json.loads(clipboard_content)
+        print("данные похожи на json. Отлично")
+        return data
+    except json.JSONDecodeError as e:
+        input("Текст в буфере не является валидным JSON попробуйте копировать ещё раз со всеми скобками.")
+        return extract_clipboard_content()
+
+
+def view_prompt(prompt: str, str_number_batch: str = "", prev_content: str = ""):
     console = Console()
-    first_gpt_promt = create_unified_prompt(words_json_path, lrc_path)
-
-    def extract_clipboard_content():
-        def get_clipboard_text():
-            """Читает текст из буфера обмена Windows."""
-            try:
-                return subprocess.check_output('powershell -command "Get-Clipboard"', text=True)
-            except Exception as e:
-                print(f"Ошибка: {e}. Убедитесь, что в буфере есть текст.")
-                return extract_clipboard_content()
-
-        console.input("[cyan]скопируйте и нажмите enter для извлечения из буфера обмена: [/cyan]")
-        clipboard_content = get_clipboard_text()
-
-        try:
-            data = json.loads(clipboard_content)
-            print("данные корректны")
-            return data
-        except json.JSONDecodeError as e:
-            print("Текст в буфере не является валидным JSON попробуйте копировать ещё раз со всеми скобками.")
-            return extract_clipboard_content()
-
-
-    def console_warn_copy():
+    try:
+        # view_warn_copy
         console.print("[red]Промпт для копировапния начинается после этих слов [/red] \n\n")
+        # view base prompt
+        print(prompt)
+        if str_number_batch:
+            console.print(f"\n\n [cyan]Порция {str_number_batch}[/cyan]")
+        # console_continue_process
+        console.input("[cyan]Отправте промпт в chatGPT, подождите ответ. "
+                      "Cкопируйте и нажмите enter для извлечения из буфера обмена: [/cyan]")
+        clipboard_content = extract_clipboard_content()
+        # return answer
+        if clipboard_content == prev_content:
+            return view_prompt(prompt, str_number_batch + "[red] Буфер обмена тот же, замените[/red], ", prev_content)
+        else:
+            return clipboard_content
+    except KeyboardInterrupt:
+        warn_ctrl_c = "[red]Получил Ctrl+C для выкл консоли, выделите нормально потом копируйте[/red]"
+        return view_prompt(prompt, str_number_batch + warn_ctrl_c, prev_content)
 
-    def console_continue_process():
-        return console.print(f"\n\n [cyan]Отправте промпт в chatGPT, подождите ответ[/cyan]")
 
+
+def semi_manual_processing(output_dir: Path, words_json_path: Path, lrc_path: Path, cards_per_batch: int = 10):
     # =========================================
     # First Prompt for gpt
     # =========================================
-    console_warn_copy()
-    print(first_gpt_promt)
-    console_continue_process()
-    answer = extract_clipboard_content()
+    first_gpt_prompt = create_unified_prompt(words_json_path, lrc_path)
+    data_anki = view_prompt(first_gpt_prompt, "initialization gpt")
 
     # Создание файла в котором будет всё лежать
     path_anki = os.path.join(output_dir, "anki.json")
     with open(path_anki, "w", encoding="utf-8") as file:
-        json.dump(answer, file)
+        json.dump(data_anki, file)
 
     # =========================================
-    # process cards
+    # Process cards
     # =========================================
 
+    config = load_config()
+    prompt_cards_gen = config.get("prompt_cards_gen", "")
     lyrics = clean_lrc_text(read_lrc_file(lrc_path))
-    total_cards = len(answer)
-    print(total_cards)
-    # for start in range(0, total_cards, cards_per_batch):
-    #     batch = cards[start:start + cards_per_batch]
-    #
-    #     cards_json = []
-    #
-    #     for card in batch:
-    #         cards_json.append({
-    #             "lemma": card.get("lemma"),
-    #             "sentence": card.get("sentence")
-    #         })
-    #
-    #     cards_str = json.dumps(
-    #         cards_json,
-    #         ensure_ascii=False,
-    #         separators=(",", ":")
-    #     )
-    #
-    #     current_batch = start // cards_per_batch + 1
-    #
-    #     total_batches = math.ceil(
-    #         total_cards / cards_per_batch
-    #     )
-    #
-    # console.print(f"[cyan]Порция {current_batch}/{total_batches}[/cyan]")
+    total_cards = len(data_anki)
+    prev_cards = ""
+
+    for start in range(0, total_cards, cards_per_batch):
+        batch = data_anki[start:start + cards_per_batch]
+
+        cards_json = []
+
+        for card in batch:
+            cards_json.append({
+                "lemma": card.get("lemma"),
+                "sentence": card.get("sentence")
+            })
+
+        # демонстрация кол-ва записей
+        current_batch = start // cards_per_batch + 1
+        total_batches = (total_cards / cards_per_batch)
+        str_number_batch = f'{current_batch}/{total_batches}'
+        ready_prompt = replace_prompt_placeholder(
+            "{song.lrc}",
+            prompt_cards_gen,
+            lyrics
+        )
+        ready_prompt = replace_prompt_placeholder(
+            "{cards.json}",
+            ready_prompt,
+            str(cards_json)
+        )
+        cards = view_prompt(ready_prompt, str_number_batch, prev_cards)
+        prev_cards = cards
+        # update data_anki
+        for i, new_card in enumerate(cards):
+            data_anki[start + i].update(new_card)
+
+    with open(path_anki, "w", encoding="utf-8") as file:
+        json.dump(data_anki, file)
